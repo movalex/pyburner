fc8f7d68933e11bf538ab9710ab98dbc38384646
Alexey Bogomolov <abogomolov@mac.com>
2017-02-08 17:45:13 +0300

    small naming improvements

--- csv_parser.py
+++ csv_parser.py
@@ -1,57 +1,61 @@
+from __future__ import print_function
 import csv
-from tkinter import filedialog, Tk
 
-root = Tk()
-root.withdraw()
+test_file = 'neon9.csv'
 
-file_path = 'neon9.csv'
 
-def parse_it(file):
-    parsed_file = csv.reader(open(file), dialect="excel-tab")
+def parse_it(file_):
+    parsed_file = csv.reader(open(file_), dialect="excel-tab")
     return parsed_file
 
-def servers_sorted(foo):
+
+def servers_sorted(file_):
     servers = []
-    for i in parse_it(foo):
+    for i in parse_it(file_):
         if len(i) == 5 and i[4] != "Server":
             servers.append(i[4])
         else:
             pass
     return sorted(set(servers))
-#print(servers_sorted(file_path))
 
-def get_job_name(foo):
+
+def get_job_name(file_):
     job_name = ''
-    for i in parse_it(foo):
+    for i in parse_it(file_):
         if len(i) == 1 and i[0][:4] == "Job:":
             job_name = i[0].split()[1]
     return job_name
-#print('Job name: {}'.format(get_job_name(file)))
 
 
 def return_frames(file_, server):
-    for _line in parse_it(file_):
+    for line_ in parse_it(file_):
         try:
-            if _line[4] == server:
-                if _line[2] == "00000":
-                    frame = _line[2][:1]
+            if line_[4] == server:
+                if line_[2] == "00000":
+                    frame = line_[2][:1]
                 else:
-                    frame = _line[2].lstrip("0")
+                    frame = line_[2].lstrip("0")
                 yield frame
             else:
                 pass
         except IndexError:
             pass
-#print(list(return_frames(file_path, 'aston3d')))
 
 
-def all_jobs(file):
-    for server in servers_sorted(file):
+def print_all_jobs(file_):
+    for server in servers_sorted(file_):
         print(server)
-        for n in return_frames(file, server):
+        for n in return_frames(file_, server):
             print(n, end=', ')
         print('\n')
 
 
+#def make_dict(file_):
+#    d = {}
+#    for i in servers_sorted(file_):
+#        d[i] = list(return_frames(file_, i))
+#    return d.items()
+
+
 if __name__ == '__main__':
-    all_jobs(file_path)
+    print_all_jobs(test_file)
