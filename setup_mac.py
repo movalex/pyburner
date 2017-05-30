f3f1d2d6982b307dbae3f86880cf2489ebd7f447
movalex <mail@abogomolov.com>
2017-05-30 04:53:54 +0300

    add version and description to mac setup

--- setup_mac.py
+++ setup_mac.py
@@ -9,6 +9,13 @@ setup(
     data_files=DATA_FILES,
     options={'py2app': OPTIONS},
     DATA_FILES=["config.ini"],
-    setup_requires=['py2app'],
+    version = '0.0.1',
+    name = 'pyBurner', 
+    description = '''
+    create .bat file
+    to submit rerender of failed server\'s frames
+    with Autodesk Backburner
+    ''',
+    setup_requires=['py2app']
 )
 
